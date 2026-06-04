12aeccf906226ea9d9f35877507958a01796b49e
Víctor Valdivia <victor.valdivia.dev@gmail.com>
2026-06-04 01:16:39 -0400

    feat(model): agregar clase base Person con validación de RUT
    Implementar la clase base Person como raíz de la jerarquía de dominio.
    Contiene atributos comunes (RUT, nombre, apellido, Address) y fuerza
    la validez del RUT a través de util/RutValidator tanto en la
    construcción como en el setter.
    
    El RUT se valida antes de la asignación, garantizando que ninguna
    instancia de Person pueda existir con un RUT inválido. Esta invariante
    se asegura lanzando InvalidRutException desde el constructor y setRut().

---
+++ src/model/Person.java
@@ -0,0 +1,101 @@
+package model;
+
+import util.*;
+
+/**
+ * Clase base del dominio que representa a una persona genérica.
+ * Contiene datos básicos como RUT, nombre, apellido y una dirección asociada
+ * mediante composición.
+ */
+public class Person {
+    private String rut;
+    private String firstName;
+    private String lastName;
+    private Address address;
+
+    /**
+     * Constructor por defecto.
+     */
+    public Person() {}
+
+    /**
+     * Constructor con todos los atributos. Valida el RUT antes de asignarlo.
+     *
+     * @param rut       RUT de la persona
+     * @param firstName nombre de pila
+     * @param lastName  apellido
+     * @param address   dirección física (composición)
+     * @throws InvalidRutException si el RUT no es válido
+     */
+    public Person(String rut, String firstName, String lastName, Address address)
+            throws InvalidRutException {
+        RutValidator.validate(rut);
+        this.rut = rut;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    /**
+     * @return RUT de la persona
+     */
+    public String getRut() {
+        return rut;
+    }
+
+    /**
+     * @param rut RUT de la persona
+     * @throws InvalidRutException si el RUT no es válido
+     */
+    public void setRut(String rut) throws InvalidRutException {
+        RutValidator.validate(rut);
+        this.rut = rut;
+    }
+
+    /**
+     * @return nombre de pila
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName nombre de pila
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return apellido
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName apellido
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return dirección física asociada
+     */
+    public Address getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address dirección física asociada
+     */
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " (RUT: " + rut + "), Dirección: " + address;
+    }
+}
